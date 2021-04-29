7f7aa6c435948b715a2aa494ea21c10b8d8d48f4
徐焱挺 <xuyanting@lidig.com>
2021-04-29 18:36:22 +0800

    上机调试

--- client.js
+++ client.js
@@ -24,18 +24,18 @@ function control(addr, value = MAP.ON) {
   const msgCrc = CRC.crc16modbus(msgNotCrc);
   const msgCrcPack = struct.pack("<H", msgCrc);
   const combine = Buffer.concat([msgNotCrc, msgCrcPack]);
-  console.log(combine.toString("hex"));
+  return combine;
 }
 
 function readCoil(startAddr, readNumber) {
   const msgWithoutCrc = struct.pack("!BBHH", 0x1, 0x1, startAddr, readNumber);
-  const crc_val = CRC.crc16modbus(msg_without_crc);
+  const crc_val = CRC.crc16modbus(msgWithoutCrc);
   const crcPack = struct.pack("<H", crc_val);
   const combine = Buffer.concat([msgWithoutCrc, crcPack]);
   console.log(combine.toString("hex"));
   return combine;
 }
-function connect(host, port, command) {
+function connect(host, port, command, cb) {
   const socket = new Socket();
   const options = {
     host,
@@ -48,31 +48,91 @@ function connect(host, port, command) {
 
   socket.on("error", console.error);
   socket.on("data", function (data) {
-    console.log("data", data);
+    console.log(command, data);
+    // const read = data.readUInt16BE(2);
+    // const status_code = Buffer.from([read]).toString("hex");
+    // cb(STATUS_MAP[status_code]);
+    socket.end();
   });
 
   socket.connect(options);
 }
 
 function up(host, port) {
-  connect(host, port, control(MAP.UP, MAP.OFF));
-  connect(host, port, control(MAP.UP, MAP.ON));
+  const socket = new Socket();
+  const options = {
+    host,
+    port,
+  };
+
+  socket.on("connect", function () {
+    socket.write(control(MAP.UP, MAP.OFF));
+    socket.write(control(MAP.UP, MAP.ON));
+  });
+
+  socket.on("error", console.error);
+  socket.on("data", function (data) {
+    console.log("up", data);
+    socket.end();
+  });
+
+  socket.connect(options);
+  // connect(host, port, control(MAP.UP, MAP.OFF));
+  // connect(host, port, control(MAP.UP, MAP.ON));
 }
+
 function down(host, port) {
-  connect(host, port, control(MAP.DOWN, MAP.OFF));
-  connect(host, port, control(MAP.DOWN, MAP.ON));
+  const socket = new Socket();
+  const options = {
+    host,
+    port,
+  };
+
+  socket.on("connect", function () {
+    socket.write(control(MAP.DOWN, MAP.OFF));
+    socket.write(control(MAP.DOWN, MAP.ON));
+  });
+
+  socket.on("error", console.error);
+  socket.on("data", function (data) {
+    console.log("down", data);
+    socket.end();
+  });
+
+  socket.connect(options);
 }
 function query(host, port) {
-  const upStatus = connect(host, port, readCoil(MAP.UP_STATUS, 1));
-  const downStatus = connect(host, port, readCoil(MAP.DOWN_STATUS, 1));
+  const upStatus = connect(host, port, readCoil(MAP.UP_STATUS, 1), (status) => {
+    console.log("up", status);
+  });
+  // const downStatus = connect(
+  //   host,
+  //   port,
+  //   readCoil(MAP.DOWN_STATUS, 1),
+  //   (status) => {
+  //     console.log("down", status);
+  //   }
+  // );
 }
 
-const buf = Buffer.alloc(6);
-buf.writeUInt16BE(0x0101, 0);
-buf.writeUInt16BE(0x0100, 2);
-buf.writeUInt16BE(0x9048, 4);
+// const buf = Buffer.alloc(6);
+// buf.writeUInt16BE(0x0101, 0);
+// buf.writeUInt16BE(0x0100, 2);
+// buf.writeUInt16BE(0x9048, 4);
 
-const read = buf.readUInt16BE(2);
-const status_code = Buffer.from([read]).toString("hex");
-console.log(buf.toString("hex"));
-console.log(STATUS_MAP[status_code]);
+// const read = buf.readUInt16BE(2);
+// const status_code = Buffer.from([read]).toString("hex");
+// console.log(buf.toString("hex"));
+// console.log(STATUS_MAP[status_code]);
+
+// up("10.10.100.254", 8899);
+
+// down("10.10.100.254", 8899);
+
+// query("10.10.100.254", 8899);
+
+// connect("10.10.100.254", 8899, control(MAP.DOWN, MAP.OFF));
+// connect("10.10.100.254", 8899, control(MAP.DOWN, MAP.ON));
+
+
+
