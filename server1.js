6a82f6c01c422ce7c29ae4dcaca973599654ab7d
徐焱挺 <xuyantiong@lidig.com>
2021-05-04 22:22:29 +0800

    server 无论如何都会返回一次参数

--- server1.js
+++ server1.js
@@ -3,9 +3,11 @@ const { createServer } = require("net");
 let globalBuf = Buffer.from([01, 01, 01, 00, 90, 48]);
 const server = createServer((socket) => {
   socket.on("data", (data) => {
+    console.log("收到数据", data);
     if (data.toString("hex") === "01050016ff006dfe") {
       // up 填值
       globalBuf = Buffer.from([01, 01, 01, 01, 90, 48]);
+      console.log("up", data);
       socket.write(data);
     }
     if (data.toString("hex") === "0101001800017dcd") {
@@ -14,9 +16,11 @@ const server = createServer((socket) => {
     }
     if (data.toString("hex") === "01050017ff003c3e") {
       // down
+      console.log("down", data);
       globalBuf = Buffer.from([01, 01, 01, 00, 90, 48]);
       socket.write(data);
     }
+    socket.write(data);
   });
 });
 
--- server4.js
+++ server4.js
@@ -3,9 +3,11 @@ const { createServer } = require("net");
 let globalBuf = Buffer.from([01, 01, 01, 00, 90, 48]);
 const server = createServer((socket) => {
   socket.on("data", (data) => {
+    console.log("收到数据", data);
     if (data.toString("hex") === "01050016ff006dfe") {
       // up 填值
       globalBuf = Buffer.from([01, 01, 01, 01, 90, 48]);
+      console.log("up", data);
       socket.write(data);
     }
     if (data.toString("hex") === "0101001800017dcd") {
@@ -14,12 +16,14 @@ const server = createServer((socket) => {
     }
     if (data.toString("hex") === "01050017ff003c3e") {
       // down
+      console.log("down", data);
       globalBuf = Buffer.from([01, 01, 01, 00, 90, 48]);
       socket.write(data);
     }
+    socket.write(data);
   });
 });
 
-server.listen(9999, () => {
-  console.log("9999");
+server.listen(9994, () => {
+  console.log("9994");
 });
