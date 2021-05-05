f693c9aaa9307a636d4f5ebf7b82827317af97d9
徐焱挺 <xuyantiong@lidig.com>
2021-05-05 21:57:57 +0800

    planb mac 32 - 10 -16 and compare

--- auth.js
+++ auth.js
@@ -34,7 +34,7 @@ function decrypt(code) {
   console.log("originalText", originalText);
   return originalText;
 }
-async function main() {
+async function planA() {
   //1. client
   const macInt = await transferMAC();
   //2. sever
@@ -44,4 +44,52 @@ async function main() {
   console.log(macInt === originalText);
 }
 
-main();
+// main();
+async function bit32MAC() {
+  const mac = await getMAC();
+  const macArray = mac.split(":");
+  const mac32 = macArray
+    .map((el) => {
+      return parseInt(el, 16).toString(32);
+    })
+    .join("-");
+  return mac32;
+}
+function parse32MACTo10(mac32) {
+  const macArray = mac32
+    .split("-")
+    .map((el) => {
+      const mac10 = parseInt(el, 32) + "";
+      const mac10Str = Math.floor(Math.random() * 10) + mac10.padStart(3, "0");
+      return mac10Str;
+    })
+    .join("-");
+  return macArray;
+}
+
+function parse10To16(mac10) {
+  const macArray = mac10
+    .split("-")
+    .map((el) => {
+      const mac16 = Number(el.substr(1, 3)).toString(16);
+      return mac16;
+    })
+    .join(":");
+
+  return macArray;
+}
+async function planB() {
+  let mac = await getMAC();
+  //1.client info
+  const mac32 = await bit32MAC();
+  console.log("mac32", mac32);
+  //2.server auth code
+  const mac10 = parse32MACTo10(mac32);
+  console.log("mac10", mac10);
+  // 3. client decrypt
+  const mac16 = parse10To16(mac10);
+  console.log("mac16", mac16);
+  console.log(mac === mac16);
+}
+
+planB();
